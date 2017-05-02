6887bcb0ef52acddffb69835ac1ef1856b42a110
David <david.quinta415@gmail.com>
2017-05-01 23:04:17 -0600

    New Images

--- app/src/main/java/quintanilla00025815/pm_parcial_1/MainActivity.java
+++ app/src/main/java/quintanilla00025815/pm_parcial_1/MainActivity.java
@@ -2,12 +2,19 @@ package quintanilla00025815.pm_parcial_1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import  android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
-
+    Toolbar tToolbar;
+    ListView cListView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tToolbar = (Toolbar) findViewById(R.id.toolbar);
+        cListView = (ListView) findViewById(R.id.countries_listView);
+
     }
 }
