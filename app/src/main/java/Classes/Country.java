b5525787607908e1de64a49ac63a5c8f59b6e4ca
David <david.quinta415@gmail.com>
2017-05-01 19:40:55 -0600

    Adding "Learning & Details" layouts

--- app/src/main/java/Classes/Country.java
+++ app/src/main/java/Classes/Country.java
@@ -11,7 +11,47 @@ public class Country {
     private String official_language;
     private Double surface;
 
-    public Country(String name, String flag, String capital, String official_language, Double surface){
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFlag() {
+        return flag;
+    }
+
+    public void setFlag(String flag) {
+        this.flag = flag;
+    }
+
+    public String getCapital() {
+        return capital;
+    }
+
+    public void setCapital(String capital) {
+        this.capital = capital;
+    }
+
+    public String getOfficial_language() {
+        return official_language;
+    }
+
+    public void setOfficial_language(String official_language) {
+        this.official_language = official_language;
+    }
+
+    public Double getSurface() {
+        return surface;
+    }
+
+    public void setSurface(Double surface) {
+        this.surface = surface;
+    }
+
+    public Country(String name, String flag, String capital, String official_language, Double surface) {
         this.name = name;
         this.flag = flag;
         this.capital = capital;
