e5db9c6eba5b920cccb210c86187fe03083ea312
hmmatus <manriquematus1@gmail.com>
2017-05-03 11:00:23 -0600

    Change of .gitignore

--- app/src/main/java/quintanilla00025815/pm_parcial_1/MainActivity.java
+++ app/src/main/java/quintanilla00025815/pm_parcial_1/MainActivity.java
@@ -1,8 +1,10 @@
 package quintanilla00025815.pm_parcial_1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 import  android.widget.ListView;
 
 import java.util.ArrayList;
@@ -12,6 +14,12 @@ import Classes.Country;
 public class MainActivity extends AppCompatActivity {
     Toolbar tToolbar;
     ListView cListView;
+
+    //Funcion que conecta con el juego
+    public void game(View v){
+        Intent game=new Intent(this,QuizActivity.class);
+        startActivity(game);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
