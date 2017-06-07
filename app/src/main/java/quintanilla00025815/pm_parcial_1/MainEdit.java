2a0af54aef677de7ac2a0e89a7472d288b82b679
lsfnts <buho9626@gmail.com>
2017-06-07 17:49:45 -0600

    w600 a w900

--- app/src/main/java/quintanilla00025815/pm_parcial_1/MainEdit.java
+++ app/src/main/java/quintanilla00025815/pm_parcial_1/MainEdit.java
@@ -3,11 +3,17 @@ package quintanilla00025815.pm_parcial_1;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import Classes.DatabaseHelperCountry;
+
 public class MainEdit extends AppCompatActivity {
+
+    DatabaseHelperCountry helperCountry;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_edit);
+
+        helperCountry = new DatabaseHelperCountry(this);
     }
 }
